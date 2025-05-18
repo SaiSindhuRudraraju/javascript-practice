2f04fd6da077a33cd443719929d3c99597a83297
Sai Sindhu Rudraraju <saisindhu2311@gmail.com>
2025-05-17 23:27:28 -0500

    basics of javascript

---
+++ website1/index.js
@@ -0,0 +1,21 @@
+
+console.log(`Hello`);
+console.log(`I Like Pizza!`);
+
+window.alert(`This is an alert!`);
+window.alert(`I like pizza!`);
+
+//This is a single line comment
+
+/*
+    This is a multi line comment
+    hi
+*/
+
+document.getElementById('myh1').textContent = `Hello`;
+
+window.alert("Observe h1 tag");
+
+document.getElementById("mypara").textContent = `This is a paragraph. I like pizza!!`;
+
+document.getElementById("myh1").innerHTML = "Changed h1 tag";
