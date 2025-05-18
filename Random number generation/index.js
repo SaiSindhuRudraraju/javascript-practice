1951ba33c19e431ffe8917d3dcb7f83bd574759e
Sai Sindhu Rudraraju <saisindhu2311@gmail.com>
2025-05-18 01:33:13 -0500

    checked property and random number generator

--- Random number generation/index.js
+++ Random number generation/index.js
@@ -11,3 +11,37 @@ mybutton.onclick = function(){
     mylable2.textContent = Math.floor(Math.random() * (max)) + min;
     mylable3.textContent = Math.floor(Math.random() * (max)) + min;
 }
+
+const mycheckbox = document.getElementById("mycheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const mastercardBtn = document.getElementById("mastercardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mysubmit = document.getElementById("mysubmit");
+const subResult = document.getElementById("subResult");
+const mysubmpaymentResultit = document.getElementById("paymentResult");
+
+mysubmit.onclick = function(){
+    if(mycheckbox.checked)
+    {
+        subResult.textContent = `You are subscribed`;
+    }
+    else{
+        subResult.textContent = `You are not subscribed`;
+    }
+
+    if(visaBtn.checked)
+    {
+        mysubmpaymentResultit.textContent = `You are paying with visa`;
+    }
+    else if(mastercardBtn.checked)
+    {
+        mysubmpaymentResultit.textContent = `You are paying with master card`;
+    }
+    else if(paypalBtn.checked)
+    {
+        mysubmpaymentResultit.textContent = `You are paying with paypal`;
+    }
+    else{
+        mysubmpaymentResultit.textContent = `You must select a payment type`;
+    }
+}
