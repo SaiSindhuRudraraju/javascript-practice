65717e57b2eed27f4e8ffe37e596b31d914e9d06
Sai Sindhu Rudraraju <saisindhu2311@gmail.com>
2025-05-23 06:06:46 -0500

    Asynchronous code Example

---
+++ Asynchronous code/index.js
@@ -0,0 +1,27 @@
+/**
+ *  Synchronous = Execute line by line consecutively in a sequential manner
+ *                Code that waits for an operation to complete.
+ * 
+ *  Asynchronous = Allows multiple operations to be performed concurrently 
+ *                 without waiting.
+ *                 Doesn't block the execution flow ans allows the program to continue
+ *                 (I/O operations, network requests, fetching data)
+ *                 Handled with: Callbacks, Promises, Async/Await
+ */
+
+//Example: setTimeout()
+
+function func1(callback)
+{
+    setTimeout( () => {console.log("Task 1"); callback();}, 3000);
+    
+}
+
+function func2()
+{
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+
+func1(func2);
